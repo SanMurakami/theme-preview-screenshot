3ee6e7225a45f2cddb8e5d85bb0b5c53e13f231c
riku6460 <17585784+riku6460@users.noreply.github.com>
2021-02-07 13:22:57 +0900

    configでいろいろ指定できるようにした

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const http = require('http');
 const fs = require('fs/promises');
 const crypto = require('crypto');
 const serviceAccount = require('./serviceAccountKey.json');
-
+const config = require('./config.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'misskeyassetstore.appspot.com'
+    storageBucket: config.firebase.storageBucket
 });
 const bucket = admin.storage().bucket();
 
@@ -40,8 +40,8 @@ http.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(
                 JSON.stringify({
-                    home: `https://firebasestorage.googleapis.com/v0/b/misskeyassetstore.appspot.com/o/theme-screenshot%2F${homeFileName}?alt=media`,
-                    note: `https://firebasestorage.googleapis.com/v0/b/misskeyassetstore.appspot.com/o/theme-screenshot%2F${noteFileName}?alt=media`
+                    home: storageFile.publicUrl(),
+                    note: storageFile.publicUrl().replace('home-', 'note-')
                 })
             );
             return;
@@ -58,14 +58,14 @@ http.createServer((req, res) => {
 
         const page = await browser.newPage();
         await page.setViewport({width: 1920, height: 1080});
-        await page.goto('https://theme-preview.misskey.io/');
+        await page.goto(config.misskey.url);
 
-        await page.evaluate(() => {
-            localStorage.setItem('accounts', '[{"id":"8h1n7osy4z","token":"82LPJVdX1ICOipKy"}]');
-            localStorage.setItem('account', '{"id":"8h1n7osy4z","name":null,"username":"preview","host":null,"avatarUrl":"http://theme-preview.misskey.io/avatar/8h1n7osy4z","avatarBlurhash":null,"avatarColor":null,"isAdmin":true,"isModerator":false,"isBot":false,"isCat":false,"emojis":[],"url":null,"createdAt":"2021-01-15T09:27:48.562Z","updatedAt":"2021-01-15T09:32:21.065Z","bannerUrl":null,"bannerBlurhash":null,"bannerColor":null,"isLocked":false,"isSilenced":false,"isSuspended":false,"description":null,"location":null,"birthday":null,"fields":[],"followersCount":0,"followingCount":0,"notesCount":4,"pinnedNoteIds":[],"pinnedNotes":[],"pinnedPageId":null,"pinnedPage":null,"twoFactorEnabled":false,"usePasswordLessLogin":false,"securityKeys":false,"avatarId":null,"bannerId":null,"injectFeaturedNote":true,"alwaysMarkNsfw":false,"carefulBot":false,"autoAcceptFollowed":true,"noCrawle":false,"isExplorable":true,"hasUnreadSpecifiedNotes":false,"hasUnreadMentions":false,"hasUnreadAnnouncement":false,"hasUnreadAntenna":false,"hasUnreadChannel":false,"hasUnreadMessagingMessage":false,"hasUnreadNotification":false,"hasPendingReceivedFollowRequest":false,"integrations":{},"mutedWords":[],"mutingNotificationTypes":[],"email":null,"emailVerified":false,"securityKeysList":[],"token":"82LPJVdX1ICOipKy"}')
-        });
+        await page.evaluate((username, id, token) => {
+            localStorage.setItem('accounts', JSON.stringify([{id, token}]));
+            localStorage.setItem('account', JSON.stringify({id, username, token}));
+        }, config.misskey.username, config.misskey.id, config.misskey.token);
 
-        await page.goto('https://theme-preview.misskey.io/settings/theme/install');
+        await page.goto(`${config.misskey.url}/settings/theme/install`);
 
         await page.waitForSelector(themeSelector, {timeout: 10000});
         await page.type(themeSelector, themeData);
@@ -74,45 +74,49 @@ http.createServer((req, res) => {
 
         await page.click('body > div > div.mvcprjjd.sidebar > nav > div > a.item.index');
 
-        await page.waitForSelector(noteSelector, {timeout: 10000})
+        await page.waitForSelector(noteSelector, {timeout: 10000});
 
         await page.waitForTimeout(400);
 
 
         try {
-
             await page.screenshot({path: homeFileName});
 
             const clip = await page.evaluate(s => {
-                const el = document.querySelector(s)
+                const el = document.querySelector(s);
 
-                const {width, height, top: y, left: x} = el.getBoundingClientRect()
-                return {width, height, x, y}
-            }, noteSelector)
+                const {width, height, top: y, left: x} = el.getBoundingClientRect();
+                return {width, height, x, y};
+            }, noteSelector);
 
             await page.screenshot({clip, path: noteFileName});
 
             await browser.close();
 
             try {
-                await bucket.upload(homeFileName, {destination: `theme-screenshot/${homeFileName}`});
-                await bucket.upload(noteFileName, {destination: `theme-screenshot/${noteFileName}`});
+                const homePromise = bucket.upload(homeFileName, {destination: `theme-screenshot/${homeFileName}`});
+                const notePromise = bucket.upload(noteFileName, {destination: `theme-screenshot/${noteFileName}`});
+                const [[home], [note]] = await Promise.all([homePromise, notePromise]);
+                await Promise.all([home.makePublic(), note.makePublic()]);
+
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(
+                    JSON.stringify({
+                        home: home.publicUrl(),
+                        note: note.publicUrl()
+                    })
+                );
+                return;
             } catch (err) {
                 console.log(err);
             }
         } finally {
-            await fs.unlink(homeFileName);
-            await fs.unlink(noteFileName);
+            await Promise.all([fs.unlink(homeFileName), fs.unlink(noteFileName)]);
         }
 
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(
-            JSON.stringify({
-                home: `https://firebasestorage.googleapis.com/v0/b/misskeyassetstore.appspot.com/o/theme-screenshot%2F${homeFileName}?alt=media`,
-                note: `https://firebasestorage.googleapis.com/v0/b/misskeyassetstore.appspot.com/o/theme-screenshot%2F${noteFileName}?alt=media`
-            })
-        );
+        res.writeHead(500);
+        res.end();
     });
 
 
-}).listen(3000);
+}).listen(config.listenPort);
